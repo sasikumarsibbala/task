effd666470af2b6c2ba1e1b63d6c6f8a4c48932c
sasikumarsibbala <65108079+sasikumarsibbala@users.noreply.github.com>
2021-07-20 01:08:07 +0530

    Delete step:stepDefinition directory

--- step:stepDefinition/give.js
+++
@@ -1,8 +0,0 @@
-const { Given} = require('@cucumber/cucumber');
-const { expect } = require('chai');
-
-Given("open url {string}",(url) => {
-    browser.url(url);
-    browser.maximizeWindow();   
-    expect(url).toEqual("/explore");
-});
