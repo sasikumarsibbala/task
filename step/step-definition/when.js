097308dee6ecd471dca04f1b07ca659441d301ce
sasikumar12860@gmail.com <sibbala.sasikumar@zucitech.com>
2021-07-25 16:12:27 +0530

    when .js updated

--- step/step-definition/when.js
+++ step/step-definition/when.js
@@ -1,21 +1,26 @@
 const { When} = require('@cucumber/cucumber');
-const { assert } = require('chai');
-const { addFeature } = require('@wdio/allure-reporter').default
+
+let before_location=0;
 When("click on the element {string}",(locator) =>{
+   var scroll=$("//h3[contains(text(),'Latest Game News')]");
+   scroll.scrollIntoView();
     var  ad=$(locator);
     ad.click();
 });
-
 When("Scroll to the element {string}",(locator)=>{
-    var  scroll=$(locator);
-    scroll.scrollIntoView();
+  var  scroll=$(locator);
+  scroll.scrollIntoView();
 });
 
 When("i save the position of element {string}",(locator)=>{
   var  comic_card=$(locator);
   before_location=Number(comic_card.getLocation('y'));
   console.log("the position before hovering is ",before_location);
+  
 });
+
+
+
 
 When("I  hover on the element {string}",(locator)=>{
      var comic_card=$(locator);
